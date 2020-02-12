486cb537fafe045592a556b3f0647a79e6f21384
Munam <munam.malik58@gmail.com>
2020-02-12 21:42:32 +0500

    Finished the weber point question (2)

--- arbisoft-2.py
+++ arbisoft-2.py
@@ -1,27 +1,54 @@
 import math
 
 
-def distance_metric(x1: int, y1: int, x2: int, y2: int):
-    print(f"x1: {x1}, y1: {y1}   ;  x2: {x2}, y2: {y2}")
-    return math.sqrt(((x1-x2) * (x1-x2)) + ((y1-y2)*(y1-y2)))
+def distance_metric(a: tuple, b: tuple):
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+
+
+def average_point(points: list):
+    avgx = 0
+    avgy = 0
+
+    for i in range(len(points)):
+        avgx += points[i][0]
+        avgy += points[i][1]
+
+    avgx = avgx/len(points)
+    avgy = avgy/len(points)
+
+    print(f"{avgx} ,  {avgy}")
+    return (avgx, avgy)
+
+
+def findMeetingPoint(points: list, avg_point: tuple):
+    avgDist = distance_metric(points[0], avg_point)
+    meetingPoint = points[0]
+    totalDistance = avgDist
+    for i in range(1, len(points)):
+        tempDist = distance_metric(points[i], avg_point)
+        totalDistance += tempDist
+        if avgDist > tempDist:
+            avgDist = tempDist
+            meetingPoint = points[i]
+    print(meetingPoint, str(totalDistance))
 
 
 with open("input2.txt", "r") as f:
     input_line = f.readline()
     distances = []
+    points = []
     while input_line[0] != '0':
         temp = []
         temp = input_line[2:].split(' ')
         temp = [int(t.strip('\n')) for t in temp]
         print(temp)
-        for i in range(0, int(input_line[0]) - 1, 2):
-            distances.append(distance_metric(temp[2*i], temp[(2*i)+1],
-                                             temp[(2*i)+2], temp[(2*i)+3]))
+        for i in range(0, int(input_line[0])):
+            points.append((temp[2*i], temp[(2*i)+1]))
+
         input_line = f.readline()
         while (input_line == '\n'):
             input_line = f.readline()
-        print(input_line)
         if input_line[0] == '0':
             break
-
-    print(distances)
+    print(points)
+    findMeetingPoint(points, average_point(points))
